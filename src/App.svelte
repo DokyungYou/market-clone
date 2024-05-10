ab3260e928ec819769804fd0e629a1e0df261b5f
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2024-05-10 22:45:18 +0900

    Feat: 로그인기능 추가
    - 구글 소셜로그인추가
    - 로그인 시 생성된 토큰값을 로컬 스토리지에 저장
    - 다른 페이지로 이동할 때마다 로그인상태인지 체크 (로그인 상태가 아니라면 로그인화면으로 이동 ())

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <!-- script내에 js, main엔 html 작성 , style엔 css
 한 파일내에 세가지 를 다 넣음-->
+<!-- 최상단 svelte파일 -->
 
 <script>
   // svelte파일을 가져오고
@@ -9,11 +10,38 @@
   import Write from "./pages/Write.svelte";
   import NOTFOUND from "./pages/NotFound.svelte";
   import Router from "svelte-spa-router"; //라우터 태그 가져오기?
+  import {
+    GoogleAuthProvider,
+    signInWithCredential,
+    getAuth,
+  } from "firebase/auth";
+  import { user$ } from "./store";
 
   import "./css/style.css";
   import "./css/header.css";
   import "./css/main.css";
   import "./css/footer.css";
+  import { onMount } from "svelte";
+
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  let isLoading = true;
+
+  const checkLogin = async () => {
+    console.log("매번 로그인상태 체크");
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) return (isLoading = true); //access토큰이 없다면 리턴
+
+    const credential = GoogleAuthProvider.credential(null, accessToken);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
+
+  //인증 제공업체에 요청하고자 하는 OAuth 2.0 범위를 지정
+  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
 
   // 해당 파일을 특정경로에 라우팅
   const routes = {
@@ -23,13 +51,19 @@
     "/write": Write,
     "*": NOTFOUND,
   };
+
+  //렌더링될때마다 checkLogin함수 실행
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
-
-<!-- <main>
-<div>와</div>
-</main> -->
+<!-- 로그인 상태가 아니라면 login페이지 보여줌 -->
+<!-- user값이 들어있지 않다면 로그인 페이지 이동-->
+{#if isLoading}<div>로딩중</div>
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
 
 <style>
 </style>
--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,4 +1,4 @@
-<form id="login-form" action="/login" method="POST">
+<!-- <form id="login-form" action="/login" method="POST">
   <div>로그인하기</div>
   <div>
     <label for="id">아이디</label>
@@ -10,4 +10,84 @@
   </div>
   <div><button type="submit">로그인</button></div>
   <div id="login_info"></div>
-</form>
+</form> -->
+
+<script>
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { user$ } from "../store";
+
+  const auth = getAuth();
+  auth.languageCode = "it";
+
+  const provider = new GoogleAuthProvider();
+
+  // 버튼 클릭시 구글로그인팝업창을 띄우기
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+
+      //모듈화 해놓은 store.js 에 있는 user$ 에 값을 세팅
+      user$.set(user);
+      localStorage.setItem("accessToken", accessToken);
+    } catch (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    }
+  };
+</script>
+
+<div>
+  <!-- 변수 앞에 $를 붙여줘야 그 안에 있는 값을 보여줄 수 있음 -->
+  <!-- 변수 뒤에 ? 붙여서 에러처리해줬음 -->
+  {#if !$user$}
+    <div>{$user$?.displayName} 로그인됨</div>
+  {/if}
+  <div>로그인하기</div>
+  <button class="login-btn-google" on:click={loginWithGoogle}>
+    <div>
+      <img
+        class="google-img"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png"
+        alt="구글마크"
+      />
+    </div>
+
+    <div>Google로 로그인하기</div>
+  </button>
+</div>
+
+<style>
+  .login-btn-google {
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+
+    width: 200px;
+    height: 50px;
+    padding: 10px, 5px, 0px, 5px;
+
+    background-color: white;
+
+    border: 1px solid gray;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .login-btn-google div {
+    margin-top: 13px;
+  }
+
+  .google-img {
+    width: 20px;
+    height: 20px;
+  }
+</style>
